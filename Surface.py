988d466bafbd98af6307b3aaed277dcfb51c3497
andrewpetrav <61365800+andrewpetrav@users.noreply.github.com>
2021-05-16 15:17:27 -0500

    Update Surface.py

--- Surface.py
+++ Surface.py
@@ -1,1 +1,11 @@
+import pygame, sys
+from pygame.locals import *
+
+WINDOWWIDTH=1000
+WINDOWHEIGHT=800
+GREEN=pygame.Color(0,255,0)
+pygame.init()
+SURFACE=pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
+SURFACE.fill(GREEN)
+square_size=100
 
